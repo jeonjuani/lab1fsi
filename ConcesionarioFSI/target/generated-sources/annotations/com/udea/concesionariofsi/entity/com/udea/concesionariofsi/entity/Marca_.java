5348c927b3f9fc565c05d8de72e70403778c1d10
jeonjuani <125917543+jeonjuani@users.noreply.github.com>
2024-09-15 00:47:16 -0500

    Add files via upload

---
+++ ConcesionarioFSI/target/generated-sources/annotations/com/udea/concesionariofsi/entity/com/udea/concesionariofsi/entity/Marca_.java
@@ -0,0 +1,17 @@
+package com.udea.concesionariofsi.entity.com.udea.concesionariofsi.entity;
+
+import com.udea.concesionariofsi.entity.com.udea.concesionariofsi.entity.Vehiculo;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-14T23:56:56")
+@StaticMetamodel(Marca.class)
+public class Marca_ { 
+
+    public static volatile SingularAttribute<Marca, String> marca;
+    public static volatile ListAttribute<Marca, Vehiculo> vehiculoes;
+    public static volatile SingularAttribute<Marca, Integer> id;
+
+}
